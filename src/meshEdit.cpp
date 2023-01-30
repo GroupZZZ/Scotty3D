6c956af3c145e66557deef34fd644327a62d95e5
kotomineshiki <31608000+kotomineshiki@users.noreply.github.com>
2023-01-29 23:02:41 -0500

    VertixFlip Finished

--- src/meshEdit.cpp
+++ src/meshEdit.cpp
@@ -51,12 +51,124 @@ FaceIter HalfedgeMesh::eraseEdge(EdgeIter e) {
 }
 
 EdgeIter HalfedgeMesh::flipEdge(EdgeIter e0) {
-  // TODO: (meshEdit)
-  // This method should flip the given edge and return an iterator to the
-  // flipped edge.
+    // TODO: (meshEdit)
+    // This method should flip the given edge and return an iterator to the
+    // flipped edge.
+      // HALFEDGES
+    HalfedgeIter h0 = e0->halfedge();
+    HalfedgeIter h1 = h0->next();
+    HalfedgeIter h2 = h1->next();
+    HalfedgeIter h3 = h0->twin();
+    HalfedgeIter h4 = h3->next();
+    HalfedgeIter h5 = h4->next();
+    HalfedgeIter h6 = h1->twin();
+    HalfedgeIter h7 = h2->twin();
+    HalfedgeIter h8 = h4->twin();
+    HalfedgeIter h9 = h5->twin();
 
-  showError("flipEdge() not implemented.");
-  return EdgeIter();
+    // VERTICES
+    VertexIter v0 = h0->vertex();
+    VertexIter v1 = h3->vertex();
+    VertexIter v2 = h8->vertex();
+    VertexIter v3 = h6->vertex();
+    // ...you fill in the rest!...
+
+    // EDGES
+    EdgeIter e1 = h5->edge();
+    EdgeIter e2 = h4->edge();
+    EdgeIter e3 = h2->edge();
+    EdgeIter e4 = h1->edge();
+
+    // ...you fill in the rest!...
+
+    // FACES
+    FaceIter f0 = h0->face();
+    FaceIter f1 = h3->face();
+    // ...you fill in the rest!...
+
+    //VertexIter v4 = mesh.newVertex();
+    // HALFEDGES
+    h0->next() = h1;
+    h0->twin() = h3;
+    h0->vertex() = v2;
+    h0->edge() = e0;
+    h0->face() = f0;
+
+    h1->next() = h2;
+    h1->twin() = h7;
+    h1->vertex() = v3;
+    h1->edge() = e3;
+    h1->face() = f0;
+
+    h2->next() = h0;
+    h2->twin() = h8;
+    h2->vertex() = v0;
+    h2->edge() = e2;
+    h2->face() = f0;
+
+    h3->next() = h4;
+    h3->twin() = h0;
+    h3->vertex() = v3;
+    h3->edge() = e0;
+    h3->face() = f1;
+
+    h4->next() = h5;
+    h4->twin() = h9;
+    h4->vertex() = v2;
+    h4->edge() = e1;
+    h4->face() = f1;
+
+    h5->next() = h3;
+    h5->twin() = h6;
+    h5->vertex() = v1;
+    h5->edge() = e4;
+    h5->face() = f1;
+
+    h6->next() = h6->next();
+    h6->twin() = h5;
+    h6->vertex() = v3;
+    h6->edge() = e4;
+    h6->face() = h6->face();
+
+    h7->next() = h7->next();
+    h7->twin() = h1;
+    h7->vertex() = v0;
+    h7->edge() = e3;
+    h7->face() = h7->face();
+
+    h8->next() = h8->next();
+    h8->twin() = h2;
+    h8->vertex() = v2;
+    h8->edge() = e2;
+    h8->face() = h8->face();
+    // ...you fill in the rest!...
+
+    // ...and don't forget about the "outside" elements!...
+    h9->next() = h9->next(); // didn't change, but set it anyway!
+    h9->twin() = h4;
+    h9->vertex() = v1;
+    h9->edge() = e1;
+    h9->face() = h9->face(); // didn't change, but set it anyway!
+
+    // VERTICES
+    v0->halfedge() = h2;
+    v1->halfedge() = h5;
+    v2->halfedge() = h4;
+    v3->halfedge() = h3;
+
+    // EDGES
+    e0->halfedge() = h0; //...you fill in the rest!...
+    e1->halfedge() = h4;
+    e2->halfedge() = h2;
+    e3->halfedge() = h1;
+    e4->halfedge() = h6;
+    // FACES
+    f0->halfedge() = h0; //...you fill in the rest!...
+    f1->halfedge() = h4;
+    //  mesh.deleteVertex(v4);
+
+    showError("flipEdge() not implemented.");
+    return e0;//EdgeIter();
 }
 
 void HalfedgeMesh::subdivideQuad(bool useCatmullClark) {
